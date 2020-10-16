a19c91035561a81711e3303ad636747ab013694b
CK <chengkai.liang@bigfishgames.com>
2020-10-16 06:54:33 -0700

    reformat the docker container run command line call

--- jenkins.sh
+++ jenkins.sh
@@ -44,8 +44,10 @@ fi
 docker container ls | grep jenkins-blueocean 2>&1 > /dev/null
 if [[ $? != 0 ]]; then
     docker container run --name jenkins-blueocean --rm --detach \
-      --network jenkins --env DOCKER_HOST=tcp://docker:2376 \
-      --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 \
+      --network jenkins \
+      --env DOCKER_HOST=tcp://docker:2376 \
+      --env DOCKER_CERT_PATH=/certs/client \
+      --env DOCKER_TLS_VERIFY=1 \
       --volume jenkins-data:/var/jenkins_home \
       --volume jenkins-docker-certs:/certs/client:ro \
       --publish 8080:8080 --publish 50000:50000 jenkinsci/blueocean
