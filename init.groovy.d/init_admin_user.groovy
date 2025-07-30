3e4ad1a65021a7a9403c7b20f768e569599d8e34
chengkai liang <chengkai.liang@costcotravel.com>
2025-07-29 18:15:51 -0700

    init_admin_user.groovy: update script to add admin user

--- init.groovy.d/init_admin_user.groovy
+++ init.groovy.d/init_admin_user.groovy
@@ -7,17 +7,15 @@ def instance = Jenkins.getInstanceOrNull()
 // Skip the setup wizard
 instance.setInstallState(InstallState.INITIAL_SETUP_COMPLETED)
 
-// Check if security is already configured
+// Configure local security realm and create admin user
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
-if (instance.securityRealm == null || !(instance.securityRealm instanceof HudsonPrivateSecurityRealm)) {
-    println "--> Configuring Jenkins Security Realm"
-    hudsonRealm.createAccount("admin", "admin123")  // username: admin, password: admin123
-    instance.setSecurityRealm(hudsonRealm)
-}
+hudsonRealm.createAccount("admin", "20Admin25")   // username: admin, password: 20Admin25
+instance.setSecurityRealm(hudsonRealm)
 
-// Authorization Strategy: Full access once logged in
+// Grant full control to logged-in users
 def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
 instance.setAuthorizationStrategy(strategy)
 
 instance.save()
-println "--> Jenkins Setup Wizard Skipped. Admin User: admin"
+println "--> Jenkins setup wizard skipped. Admin user 'admin' created."
+
